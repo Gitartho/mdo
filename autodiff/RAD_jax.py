657764741767b03154adf6d44e63c2ecf499b9e2
Gitartha Bharadwaj <gitartha@utk.edu>
2024-10-07 18:34:44 -0400

    oct 7

--- autodiff/RAD_jax.py
+++ autodiff/RAD_jax.py
@@ -2,22 +2,39 @@ import jax
 from jax import grad
 import jax.numpy as jnp
 
-def func(x,a):
+# ============================================================================#
+# def func(x,a):
     
-    y1 = x[0] * x[1] + x[2] * (x[0] + x[1])
-    y2 = x[0] * x[1] + x[2] * (x[0] + x[1])
+#     y1 = x[0] * x[1] + x[2] * (x[0] + x[1])
+#     y2 = x[0] * x[1] + x[2] * (x[0] + x[1])
     
-    return [y1, a*y2]
+#     return [y1, a*y2]
 
-x = jnp.array([1.0, 2.0, 3.0])
-# gradient_func = grad(func)
-# gradients = gradient_func(x,1)
-# print(f"Gradients via grad():{gradients}")
+# x = jnp.array([1.0, 2.0, 3.0])
+# # gradient_func = grad(func)
+# # gradients = gradient_func(x,1)
+# # print(f"Gradients via grad():{gradients}")
 
-gradient_func = jax.jacfwd(func)
-gradients = gradient_func(x,2)
-print(f"Gradients via jacfwd:{gradients}")
+# gradient_func = jax.jacfwd(func)
+# gradients = gradient_func(x,2)
+# print(f"Gradients via jacfwd:{gradients}")
 
-gradient_func = jax.jacrev(func)
-gradients = gradient_func(x,2)
-print(f"Gradients via jacrev:{gradients}")
+# gradient_func = jax.jacrev(func)
+# gradients = gradient_func(x,2)
+# print(f"Gradients via jacrev:{gradients}")
+
+# ============================================================================#
+def func(x):
+    
+    y1 = 1/x[0]**2
+    y2 = 1/x[1]**2
+    
+    F = jnp.array([[y1, 0], [0, y2]])
+    G = jnp.linalg.inv(F)
+    
+    return G.flatten()
+
+x = jnp.array([1.0, 2.0])
+
+gradient = jax.jacfwd(func, argnums=0)(x)
+print(gradient)
--- autodiff/adjoint3.py
+++ autodiff/adjoint3.py
@@ -5,16 +5,18 @@ from jax import numpy as np
 
 class system():
     
+    #=========================================================================#
     def __init__(self, F, Nx, Nt, param_dict):
         
         self.F = F #transition matrices
         self.Nx = Nx
         self.Nt = Nt
-        self.u = np.zeros([Nx, Nt])    
+        self.x = np.zeros([Nx, Nt])
         
         self.x0 = param_dict["Xinit"]
         self.dt = param_dict["timestep"]
-        
+    
+    #=========================================================================#
     def residual(self, x, u):
         
         Nx = self.Nx
@@ -23,20 +25,23 @@ class system():
         dt = self.dt
         F = self.F
         
-        res = np.array([Nt*Nx, 1])
-        xprev = np.append(x0, x)
+        x = np.reshape(x, (Nx, Nt), order='F')
+        
+        res = np.ones([Nt*Nx,])
+        xprev = np.append(x0, x, axis=1)
         
         for i in range(Nt):
             
             j = 4*i
-            # F = np.array([[u[j]  , u[j+1]], 
-            #               [u[j+2], u[j+3]]])
+            F = np.array([[u[j]  , u[j+1]], 
+                          [u[j+2], u[j+3]]])
             
             val = x[:,i] - xprev[:,i] - dt * F @ xprev[:,i]
             res = res.at[Nx*i : Nx*(i+1)].set(val)
-            
+        
         return res
     
+    #=========================================================================#
     def solve(self, u, tol=1e-6, max_iter=100):
         
         Nx = self.Nx
@@ -47,7 +52,7 @@ class system():
         # Solving r(x, u) is a root finding operation.
         # Note the dimensions; x -> Nx X Nt; residual -> Nx*Nt X 1
         
-        x = np.ones([Nx, Nt])
+        x = np.ones([Nx*Nt,])
         
         for i in range(max_iter):
             
@@ -55,22 +60,67 @@ class system():
 
             if np.linalg.norm(res, ord=2) < tol:
                 print(f"Converged after {i} iterations.")
-                return x
+                x_sol = np.reshape(x, (Nx, Nt), order='F')
+                self.x = x_sol
+                return x_sol
             
             Jac = jax.jacfwd(self.residual, argnums=0)
             dr_dx = Jac(x,u)
+            
+            print(dr_dx.shape)
 
-            dx = jnp.linalg.solve(dr_dx, -res)
+            dx = np.linalg.solve(dr_dx, -res)
             x = x + dx
 
         raise ValueError(
             "Newton's method did not converge after the maximum number of iterations.")
-
-F = np.arrray([[2,0],[0,2]])
-x0 = np.arrray([[1],[1]])
-dt = 1
-
-param = {}
-
     
-system()
+    #=========================================================================#
+    def objective(self, x, u):
+        
+        Nx = self.Nx
+        Nt = self.Nt
+        
+        obj = np.linalg.norm(x, ord=1)
+        
+        return obj
+    
+#=============================================================================#
+" Compute d(objctive)/d(control u) "
+
+def adjoint(sys_obj, u):
+    
+    x = sys_obj.x.flatten()
+    
+    dr_dx = jax.jacrev(sys_obj.residual, argnums=0)(x,u)
+    dr_du = jax.jacrev(sys_obj.residual, argnums=1)(x,u)
+    
+    print(f"dr_dx {dr_dx.shape}; dr_du {dr_du.shape}")
+    
+    phi = np.linalg.solve(dr_dx, dr_du)
+    
+    df_dx = jax.jacrev(sys_obj.objective, argnums=0)(x,u)
+    df_du = jax.jacrev(sys_obj.objective, argnums=1)(x,u)
+    
+    Df_Du = df_du - df_dx @ phi
+
+    print("Direct method:", Df_Du)
+
+    "======== Adjoint calculations ========="
+    psi = np.linalg.solve(np.array(dr_dx).T, df_dx)
+    Df_Du = df_du - psi.T @ dr_du
+
+    print("Adjoint method:", Df_Du)
+    
+#=============================================================================#
+
+F = np.array([[2,0],[0,2]])
+x0 = np.array([[1.0],[1.0]])
+dt = 1
+param = {"Xinit":x0, "timestep":dt}
+u = np.array([10.0])
+    
+sys_obj = system(F, 2, 10, param)
+
+adjoint(sys_obj, u)
+
